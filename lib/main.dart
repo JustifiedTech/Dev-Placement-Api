3241032a596054ae1adf92b0da2ae4a049be08f0
Kelechi Brian <kcsimpleslim@gmail.com>
2021-01-13 18:19:57 +0100

    first commit in github cars app

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import 'package:http/http.dart';
+import 'package:csv/csv.dart';
+
+import './homePage.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Cars App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Car Display'),
+    );
+  }
+}
